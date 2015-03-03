63d5f003bf7d41446b7576eed3a5ded6d38505d9
Jinglei Ren <jinglei@ren.systems>
2015-03-03 02:08:58 -0800

    [tpcc-mysql] Stores user and passward in variables. Specifies B-tree index.

--- tpcc-mysql/run.sh
+++ tpcc-mysql/run.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+USER=root
+PASSWORD=root
+
 NUM_WAREHOUSES=20
 NUM_CLIENTS=8
 RAMPUP_TIME=10 # sec
@@ -7,19 +10,19 @@ MEASURE_TIME=1200 #sec
 
 cd tpcc-mysql
 
-mysql -u root -p -e "DROP DATABASE IF EXISTS tpcc; CREATE DATABASE tpcc;"
-mysql -u root -p tpcc < create_table_memory.sql
-mysql -u root -p tpcc < add_fkey_idx.sql
+mysql -u $USER -p$PASSWORD -e "DROP DATABASE IF EXISTS tpcc; CREATE DATABASE tpcc;"
+mysql -u $USER -p$PASSWORD tpcc < create_table_memory.sql
+mysql -u $USER -p$PASSWORD tpcc < add_fkey_idx_memory.sql
 
-./tpcc_load 127.0.0.1 tpcc root "" $NUM_WAREHOUSES
-./tpcc_start -h127.0.0.1 -dtpcc -uroot -w$NUM_WAREHOUSES -c$NUM_CLIENTS -r$RAMPUP_TIME -l$MEASURE_TIME > tpcc-output-memory.log
+./tpcc_load 127.0.0.1 tpcc $USER $PASSWORD $NUM_WAREHOUSES
+./tpcc_start -h127.0.0.1 -dtpcc -u$USER -p$PASSWORD -w$NUM_WAREHOUSES -c$NUM_CLIENTS -r$RAMPUP_TIME -l$MEASURE_TIME > tpcc-output-memory.log
 
-mysql -u root -p -e "DROP DATABASE IF EXISTS tpcc; CREATE DATABASE tpcc;"
-mysql -u root -p tpcc < create_table.sql
-mysql -u root -p tpcc < add_fkey_idx.sql
+mysql -u $USER -p$PASSWORD -e "DROP DATABASE IF EXISTS tpcc; CREATE DATABASE tpcc;"
+mysql -u $USER -p$PASSWORD tpcc < create_table.sql
+mysql -u $USER -p$PASSWORD tpcc < add_fkey_idx.sql
 
-./tpcc_load 127.0.0.1 tpcc root "" $NUM_WAREHOUSES
-./tpcc_start -h127.0.0.1 -dtpcc -uroot -w$NUM_WAREHOUSES -c$NUM_CLIENTS -r$RAMPUP_TIME -l$MEASURE_TIME > tpcc-output.log
+./tpcc_load 127.0.0.1 tpcc $USER $PASSWORD $NUM_WAREHOUSES
+./tpcc_start -h127.0.0.1 -dtpcc -u$USER -p$PASSWORD -w$NUM_WAREHOUSES -c$NUM_CLIENTS -r$RAMPUP_TIME -l$MEASURE_TIME > tpcc-output.log
 
 ./tpcc-output-analyze.sh tpcc-output-memory.log > tpcc-memory-data.txt
 ./tpcc-output-analyze.sh tpcc-output.log > tpcc-data.txt
--- tpcc-mysql/setup.sh
+++ tpcc-mysql/setup.sh
@@ -11,5 +11,6 @@ cd tpcc-mysql/
 make -C src
 
 sed "s/Engine=InnoDB/Engine=MEMORY/g" create_table.sql > create_table_memory.sql
-sed -i "s/ text,/ varchar(8192),/g" create_table_memory.sql
+sed -i "s/ text,/ varchar(2048),/g" create_table_memory.sql
+sed "s/ ON / USING BTREE ON /g" add_fkey_idx.sql > add_fkey_idx_memory.sql
 
